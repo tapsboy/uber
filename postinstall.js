f2be88d8486862c6966fcdd48c54db18ac0cdb4e
Tapan Shah <tapsboy@gmail.com>
2016-03-27 23:07:08 -0400

    Add postinstall message

---
+++ postinstall.js
@@ -0,0 +1,5 @@
+console.log(
+  "This is a dummy package that does something silly.\n" +
+  "Were you expecting a package related to Uber, the car sharing service?\n" + 
+  "If yes, do a +1 here: https://github.com/tapsboy/uber/issues/1"
+);
